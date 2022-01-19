786cb230830ae6e8f2a62c149dae7ead1618bfe0
ldscavo <logan@scavo.dev>
2022-01-18 23:21:00 -0500

    figuring out ways to write expecto tests

--- src/GiftBudget.Tests/Example.fs
+++ src/GiftBudget.Tests/Example.fs
@@ -3,7 +3,11 @@
 open Expecto
 
 [<Tests>]
-let tests = testList "Server" [
+let tests = testList "Example tests" [
     testCase "one is one" <| fun _ ->
-        Expect.equal 1 1 "One is one"    
+        Expect.equal 1 1 "One is one"
+
+    test "Another test example" {
+        Expect.equal 1 1 "This is done in a computation expression"
+    }
 ]
