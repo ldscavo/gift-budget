641176da11de09010622092f395a005cc4ebda91
ldscavo <logan@scavo.dev>
2022-01-23 18:45:13 -0500

    more tests

--- src/GiftBudget.Tests/Shared/Database.Tests.fs
+++ src/GiftBudget.Tests/Shared/Database.Tests.fs
@@ -9,12 +9,12 @@ let tests = testList "Shared Database tests" [
     testList "Connection string parsing tests" [
         test "A correct uri-style postgres connection string is convered to Windows-style" {
             let uri = "postgres://username:password@localhost:1234/database"
-            let conn = connString uri
+            let maybeConn = connString uri
 
-            Expect.isSome conn "The uri parsing was successful"
+            Expect.isSome maybeConn "The uri parsing was successful"
             
             let expectedValue = "Host=localhost;Port=1234;Username=username;Password=password;Database=database"
-            let value = conn |> Option.defaultValue ""
+            let value = maybeConn |> Option.defaultValue ""
             Expect.equal expectedValue value "The resulting connection string has all the correct parts"
         }
 
@@ -25,11 +25,14 @@ let tests = testList "Shared Database tests" [
             "postgres://username:password@localhost/database"
             "postgres://username:password@localhost:1234"
             "username:password@localhost:1234/database"
+            "bork"
+            ""
+            null
         ]
         |> testParams "A uri without all the values results in a failure" (fun uri ->
-            let conn = connString uri
+            let maybeConn = connString uri
 
-            Expect.isNone conn "The uri parsing was not successful"
+            Expect.isNone maybeConn "The uri parsing was not successful"
         )
     ]
 ]
