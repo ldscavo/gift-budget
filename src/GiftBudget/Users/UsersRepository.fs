b85cfbfed16a7057d4a4eba9dd6f86a9f594369b
ldscavo <logan@scavo.dev>
2022-01-25 22:50:23 -0500

    make user repo a top-level module

--- src/GiftBudget/Users/UsersRepository.fs
+++ src/GiftBudget/Users/UsersRepository.fs
@@ -1,79 +1,78 @@
-namespace Users
+module Users.Database
 
 open Database
 open System.Threading.Tasks
 open FSharp.Control.Tasks.ContextInsensitive
 open Npgsql
 
-module Database =
-    let getAll connectionString =
-        task {
-            use connection = new NpgsqlConnection(connectionString)
-            let sql = """
-                SELECT
-                    id, email, password, is_admin,
-                    created_on, updated_on
-                FROM Users;"""
+let getAll connectionString =
+    task {
+        use connection = new NpgsqlConnection(connectionString)
+        let sql = """
+            SELECT
+                id, email, password, is_admin,
+                created_on, updated_on
+            FROM Users;"""
 
-            return! query connection sql None
-        }
+        return! query connection sql None
+    }
 
-    let getById connectionString id : Task<Result<User option, exn>> =
-        task {
-            use connection = new NpgsqlConnection(connectionString)
-            let query = """
-                SELECT
-                    id, email, password, is_admin,
-                    created_on, updated_on
-                FROM Users
-                WHERE id = @id;"""
+let getById connectionString id : Task<Result<User option, exn>> =
+    task {
+        use connection = new NpgsqlConnection(connectionString)
+        let query = """
+            SELECT
+                id, email, password, is_admin,
+                created_on, updated_on
+            FROM Users
+            WHERE id = @id;"""
 
-            return! querySingle connection query (dict [ "id" => id ] |> Some)
-        }
+        return! querySingle connection query (dict [ "id" => id ] |> Some)
+    }
 
-    let getByEmail connectionString (email: string) : Task<Result<User option, exn>> =
-        task {
-            use conn = new NpgsqlConnection(connectionString)
-            let query = """
-                select
-                    id, email, password,
-                    is_admin, created_on, updated_on
-                FROM Users
-                WHERE email = @email; """
+let getByEmail connectionString (email: string) : Task<Result<User option, exn>> =
+    task {
+        use conn = new NpgsqlConnection(connectionString)
+        let query = """
+            select
+                id, email, password,
+                is_admin, created_on, updated_on
+            FROM Users
+            WHERE email = @email; """
 
-            return! querySingle conn query (dict [ "email" => email ] |> Some)
-        }
+        return! querySingle conn query (dict [ "email" => email ] |> Some)
+    }
 
-    let update connectionString v : Task<Result<int, exn>> =
-        task {
-            use connection = new NpgsqlConnection(connectionString)
-            let query = """
-                UPDATE Users
-                SET email = @email,
-                    password = @password,
-                    is_admin = @is_admin,
-                    created_on = @created_on,
-                    updated_on = @updated_on
-                WHERE id = @id; """
+let update connectionString v : Task<Result<int, exn>> =
+    task {
+        use connection = new NpgsqlConnection(connectionString)
+        let query = """
+            UPDATE Users
+            SET email = @email,
+                password = @password,
+                is_admin = @is_admin,
+                created_on = @created_on,
+                updated_on = @updated_on
+            WHERE id = @id; """
 
-            return! execute connection query v
-        }
+        return! execute connection query v
+    }
 
-    let insert connectionString v : Task<Result<int, exn>> =
-        task {
-            use connection = new NpgsqlConnection(connectionString)
-            let query = """
-                INSERT INTO Users
-                    (id, email, password, is_admin, created_on, updated_on)
-                VALUES
-                    (@id, @email, @password, @is_admin, @created_on, @updated_on); """
+let insert connectionString v : Task<Result<int, exn>> =
+    task {
+        use connection = new NpgsqlConnection(connectionString)
+        let query = """
+            INSERT INTO Users
+                (id, email, password, is_admin, created_on, updated_on)
+            VALUES
+                (@id, @email, @password, @is_admin, @created_on, @updated_on); """
 
-            return! execute connection query v
-        }
+        return! execute connection query v
+    }
 
-    let delete connectionString id : Task<Result<int, exn>> =
-        task {
-            use connection = new NpgsqlConnection(connectionString)
+let delete connectionString id : Task<Result<int, exn>> =
+    task {
+        use connection = new NpgsqlConnection(connectionString)
 
-            return! execute connection "DELETE FROM Users WHERE id = @id;" (dict [ "id" => id ])
-        }
+        return! execute connection "DELETE FROM Users WHERE id = @id;" (dict [ "id" => id ])
+    }
