942e212e6622ce982332ee758f67330ee2995af0
ldscavo <logan@scavo.dev>
2022-02-03 20:52:33 -0500

    Add a login form type and make the validation work on both

--- src/GiftBudget/Users/UsersModel.fs
+++ src/GiftBudget/Users/UsersModel.fs
@@ -1,14 +1,29 @@
 namespace Users
 
+type ILogin =
+    abstract email: string
+    abstract password: string
+
 [<CLIMutable>]
-type User = {
-    id: System.Guid
-    email: string
-    password: string
-    is_admin: bool
-    created_on: System.DateTime
-    updated_on: System.DateTime
-}
+type User =
+    { id: System.Guid
+      email: string
+      password: string
+      is_admin: bool
+      created_on: System.DateTime
+      updated_on: System.DateTime }
+
+    interface ILogin with
+        member x.email = x.email
+        member x.password = x.password
+
+type Login =
+    { email: string
+      password: string }
+
+    interface ILogin with
+        member x.email = x.email
+        member x.password = x.password
 
 module Validation =    
     open System
@@ -16,13 +31,13 @@ module Validation =
 
     let emailRegex = Regex("\w+@\w+\.\w+")
 
-    let validate user =
+    let validate (user: ILogin) =
         let validators = [
-            fun u ->
+            fun (u: ILogin) ->
                 if String.IsNullOrWhiteSpace u.password then Some ("password", "Password shouldn't be empty")
                 else None
 
-            fun u ->
+            fun (u: ILogin) ->
                 if emailRegex.IsMatch u.email then None
                 else Some ("email", "Email address is invalid")
         ]
