42118f176c4ddf5d1df44650d6e0553a53e42981
ldscavo <logan@scavo.dev>
2022-04-10 22:48:26 -0400

    Add db table for Recipients

---
+++ src/Migrations/202204102226.Recipient.fs
@@ -0,0 +1,22 @@
+namespace Migrations
+open SimpleMigrations
+
+[<Migration(202204102226L, "Create Recipients")>]
+type CreateRecipients() =
+  inherit Migration()
+
+  override __.Up() =
+    base.Execute(@"CREATE TABLE Recipients(
+      id UUID NOT NULL,
+      user_id UUID NOT NULL,
+      name TEXT NOT NULL,
+      notes TEXT NOT NULL,
+      created_on TIMESTAMP NOT NULL,
+      updated_on TIMESTAMP NOT NULL,
+
+      PRIMARY KEY (id),
+      FOREIGN KEY (user_id) REFERENCES users (id)
+    )")
+
+  override __.Down() =
+    base.Execute(@"DROP TABLE Recipients")
