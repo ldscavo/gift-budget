6d610e9b5bf80d6f63a992e180496a0474c633b4
ldscavo <logan@scavo.dev>
2022-04-10 22:19:14 -0400

    Barebones styling setup for now

--- src/GiftBudget/Templates/App.fs
+++ src/GiftBudget/Templates/App.fs
@@ -2,25 +2,28 @@ module App
 
 open Giraffe.ViewEngine
 
-let layout (content: XmlNode list) =
+let layout content =
     html [] [
         head [] [
             meta [_charset "utf-8"]
-            meta [_name "viewport"; _content "width=device-width, initial-scale=1" ]
+            meta [_name "viewport"; _content "width=device-width, initial-scale=1"]
             title [] [str "Gift Budget"]
-            link [_rel "stylesheet"; _href "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" ]
-            link [_rel "stylesheet"; _href "https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css" ]
-            link [_rel "stylesheet"; _href "/app.css" ]
+            link [_rel "icon"; _href "/favicon.ico"]
+            link [_rel "stylesheet"; _href "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"]
+            link [_rel "stylesheet"; _href "https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css"]
+            link [_rel "stylesheet"; _href "/app.css"]
         ]
         body [] [
-            nav [_class "navbar"] [
+            nav [_id "nav"; _class "navbar"] [
                 div [_class "navbar-brand"] [
-                    a [_class "navbar-item"; _href "/"] [str "Gift Budget"]
+                    a [_class "navbar-item"; _href "/"] [
+                        img [_src "/logo.png"]
+                        str "Gift Budget"
+                    ]
                 ]
                 div [_class "navbar-menu"] [
-                    div [_class "navbar-start"] [
-                        a [_class "navbar-item"] [str "Recipients"]
-                        a [_class "navbar-item"] [str "Budgets"]
+                    div [_class "navbar-end"] [
+                        a [_class "navbar-item"] [str "Login"]
                     ]
                 ]
             ]
@@ -35,7 +38,7 @@ let layout (content: XmlNode list) =
                     ]
                 ]                
                 div [_class "column is-four-fifths"] [
-                    div [_class "container"; _style "border: 1px solid #000"]
+                    div [_class "container"]
                         content
                 ]
             ]
