acbb0d6eb52b96364fa4bf4756f419207c9f633e
ldscavo <logan@scavo.dev>
2022-04-10 22:19:36 -0400

    Initial domain model for Recipient

---
+++ src/GiftBudget/Recipients/RecipientsModel.fs
@@ -0,0 +1,37 @@
+﻿namespace Recipients
+
+open System
+
+type IRecipient =
+    abstract Name: string
+    abstract Notes: string
+
+[<CLIMutable>]
+type Recipient =
+    { Id: Guid
+      Name: string
+      Notes: string
+      CreatedOn: DateTime
+      UpdatedOn: DateTime }
+
+    interface IRecipient with
+        member this.Name = this.Name
+        member this.Notes = this.Notes
+
+module Validation =
+    let validate (recipient: IRecipient) =
+        let validators = [
+            fun (g: IRecipient) ->
+                if String.IsNullOrEmpty g.Name then
+                    Some ("name", "Recipient must have a name")
+                else
+                    None
+        ]
+
+        validators
+        |> List.fold
+            (fun acc validation ->
+                match validation recipient with
+                | Some (key, msg) -> acc |> Map.add key msg
+                | None -> acc)
+            Map.empty
