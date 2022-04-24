6c11fe71b9b73741735f7aa7588f40cddff77828
ldscavo <logan@scavo.dev>
2022-04-24 17:17:27 -0400

    Made the login page not use the shared layout

--- src/GiftBudget/Templates/App.fs
+++ src/GiftBudget/Templates/App.fs
@@ -2,17 +2,20 @@ module App
 
 open Giraffe.ViewEngine
 
+let layoutHead =
+    head [] [
+        meta [_charset "utf-8"]
+        meta [_name "viewport"; _content "width=device-width, initial-scale=1"]
+        title [] [str "Gift Budget"]
+        link [_rel "icon"; _href "/favicon.ico"]
+        link [_rel "stylesheet"; _href "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"]
+        link [_rel "stylesheet"; _href "https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css"]
+        link [_rel "stylesheet"; _href "/app.css"]
+    ]
+
 let layout content =
     html [] [
-        head [] [
-            meta [_charset "utf-8"]
-            meta [_name "viewport"; _content "width=device-width, initial-scale=1"]
-            title [] [str "Gift Budget"]
-            link [_rel "icon"; _href "/favicon.ico"]
-            link [_rel "stylesheet"; _href "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"]
-            link [_rel "stylesheet"; _href "https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css"]
-            link [_rel "stylesheet"; _href "/app.css"]
-        ]
+        layoutHead
         body [] [
             nav [_id "nav"; _class "navbar"] [
                 div [_class "navbar-brand"] [
--- src/GiftBudget/Users/UsersViews.fs
+++ src/GiftBudget/Users/UsersViews.fs
@@ -1,6 +1,5 @@
 module Users.Views
 
-open Microsoft.AspNetCore.Http
 open Giraffe.ViewEngine
 open Saturn
 
@@ -15,36 +14,45 @@ let login ctx login formErrors redirectUrl =
             p [ _class "help is-danger" ] [ str formErrors.[key] ]
         else span [] []
 
-    App.layout [
-        div [ _class "container" ] [
-            h1 [_class "is-size-3"] [ str "Login" ]
-            div [_class "box"] [
-                form [_class "control"; _action (Links.index ctx); _method "post"] [                    
-                    div [_class "field"] [
-                        label [_class "label"] [str "Email"]                        
-                        input [
-                            _class "input"; _name "email"; _placeholder "user@example.com"
-                            _value (login |> getOrDefault (fun l -> l.email) "")
-                        ]                        
-                        maybeError "email"
-                    ]
-                    div [_class "field"] [
-                        label [_class "label"] [str "Password"]                        
-                        input [_class "input"; _type "password"; _name "password"]                        
-                        maybeError "password"
-                    ]
-                    div [_class "field"] [
-                        input [
-                            _type "hidden"; _name "redirectUrl"
-                            _value (redirectUrl |> Option.defaultValue "/")
+    html [_style "background: #42babd"] [
+        App.layoutHead
+        body [_id "login-page"] [
+            div [_class "container mt-6"] [
+                div [ _class "columns is-centered" ] [
+                    div [_class "column is-half"] [
+                        div [_class "box"] [
+                            h1 [_class "is-size-3 has-text-centered"] [
+                                img [_src "/logo.png"]
+                                str "Gift Budget"
+                            ]
+                            form [_class "control"; _action (Links.index ctx); _method "post"] [                    
+                                div [_class "field"] [
+                                    label [_class "label"] [str "Email"]                        
+                                    input [
+                                        _class "input"; _name "email"; _placeholder "user@example.com"
+                                        _value (login |> getOrDefault (fun l -> l.email) "")
+                                    ]                        
+                                    maybeError "email"
+                                ]
+                                div [_class "field"] [
+                                    label [_class "label"] [str "Password"]                        
+                                    input [_class "input"; _type "password"; _name "password"]                        
+                                    maybeError "password"
+                                ]
+                                div [_class "field"] [
+                                    input [
+                                        _type "hidden"; _name "redirectUrl"
+                                        _value (redirectUrl |> Option.defaultValue "/")
+                                    ]
+                                    input [ _class "button is-link"; _type "submit"; _value "Login" ]
+                                ]                        
+                            ]
                         ]
-                        input [ _class "button is-link"; _type "submit"; _value "Login" ]
-                    ]                        
+                    ]
                 ]
             ]
         ]
-        
-    ]
+    ]    
 
 let loginSuccess ctx =
     str "HUZZAH"
