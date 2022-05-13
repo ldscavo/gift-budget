6c650c0df0d87f4fc40ad52ddc4ac01fe700d9f2
ldscavo <logan@scavo.dev>
2022-05-13 16:01:06 -0400

    Flesh out the recipient card somewhat

--- src/GiftBudget/Recipients/RecipientsViews.fs
+++ src/GiftBudget/Recipients/RecipientsViews.fs
@@ -2,7 +2,33 @@
 
 open Giraffe.ViewEngine
 
-let recipientsList ctx (recipients: seq<Recipient>) =
+let recipientCard (recipient: Recipient) =
+    div [_class "tile"] [
+        div [_class "card"] [
+            div [_class "card-content"] [
+                div [_class "media"] [
+                    div [_class "media-content"] [
+                        p [_class "title is-4"] [str recipient.Name]
+                    ]
+                ]
+                match recipient.Notes with
+                | Some notes -> div [_class "content"] [ str notes ]
+                | None -> span [] []
+            ]        
+            div [_class "card-footer"] [
+                a [_class "card-footer-item"; _href "#"] [str "View Details"]
+                a [_class "card-footer-item"; _href "#"] [str "Add New Idea"]
+            ]
+        ]
+    ]
+
+let recipientsList ctx (recipients: Recipient list) =
+    let recipientCardList = 
+        match recipients with
+        | [] -> [ div [] [str "You've not added any recipients yet!"] ]
+        | _ -> recipients |> List.map recipientCard 
+
     App.layout [
-        div [] [str "Recipients list goes here!"]
+        h1 [_class "title"] [str "Recipients"]
+        div [] recipientCardList                  
     ]
