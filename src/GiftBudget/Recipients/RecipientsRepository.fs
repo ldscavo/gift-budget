b5e1606eff21dbc31453e75685a6d84e56b90619
ldscavo <logan@scavo.dev>
2022-05-14 18:01:42 -0400

    Add insert function to recipeints repository

--- src/GiftBudget/Recipients/RecipientsRepository.fs
+++ src/GiftBudget/Recipients/RecipientsRepository.fs
@@ -6,19 +6,29 @@ open System.Threading.Tasks
 open FSharp.Control.Tasks
 open Npgsql
 
-type private RecipientDto =
+type RecipientDataEntity =
     { id: Guid
       name: string
       notes: string
       created_on: DateTime
       updated_on: DateTime }
 
-let private toRecipient(r: RecipientDto) =
+let toRecipient(r: RecipientDataEntity) =
     { Id = r.id
       Name = r.name
-      Notes = if (String.IsNullOrWhiteSpace r.notes) then (Some r.notes) else None
+      Notes = if (String.IsNullOrWhiteSpace r.notes) then None else (Some r.notes)
       CreatedOn = r.created_on
       UpdatedOn = r.updated_on }
+
+let fromRecipient (r: Recipient) =
+    { id = r.Id
+      name = r.Name
+      notes =
+        match r.Notes with
+        | Some note -> note
+        | None -> null
+      created_on = r.CreatedOn
+      updated_on = r.UpdatedOn }
 
 let getAllForUser connString (userId: Guid) =
     task {
@@ -44,4 +54,17 @@ let getById connString (id: Guid) =
 
         let! recipients = querySingle connection sql (dict ["id" => id] |> Some)        
         return recipients |> Result.map (Option.map toRecipient)
+    }
+
+let insert connectionString (recipient: Recipient) : Task<Result<int, exn>> =
+    task {
+        use connection = new NpgsqlConnection(connectionString)
+        let sql = """
+            INSERT INTO Recipients
+                (id, name, notes, created_on, updated_on)
+            VALUES
+                (@id, @name, @notes, created_on, updated_on)
+        """
+
+        return! execute connection sql (recipient |> fromRecipient)
     }
