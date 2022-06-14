29e10be7efa06e32e8daa83b18fc51058a73d29a
ldscavo <logan@scavo.dev>
2022-06-13 22:45:40 -0400

    Add an initial repository query

--- src/GiftBudget/Ideas/IdeasRepository.fs
+++ src/GiftBudget/Ideas/IdeasRepository.fs
@@ -2,9 +2,7 @@
 
 open System
 open Database
-open System.Threading.Tasks
 open FSharp.Control.Tasks
-open Npgsql
 
 [<CLIMutable>]
 type IdeaDataEntity =
@@ -22,9 +20,35 @@ let toIdea i =
       Text = i.text
       Price = Option.ofNullable i.price
       Link =
-        match String.IsNullOrWhiteSpace i.link with
-        | false -> Some i.link
-        | true -> None
+          match String.IsNullOrWhiteSpace i.link with
+          | false -> Some i.link
+          | true -> None
       CreatedOn = i.created_on
       UpdatedOn = i.updated_on
       Recipient = NoRecipient }
+
+let fromIdea i =
+    { id = i.Id
+      user_id = i.UserId
+      text = i.Text
+      price = Option.toNullable i.Price
+      link =
+          match i.Link with
+          | Some link -> link
+          | None -> null
+      created_on = i.CreatedOn
+      updated_on = i.UpdatedOn }
+
+let getAllForRecipient (env: #IDb) (recipientId: Guid) =
+    task {
+        let sql = """
+            SELECT id, user_id, text, price, link, created_on, updated_on
+            FROM Ideas
+            JOIN IdeaRecipients ON
+                Idea.id = IdeaRecipients.idea_id AND
+                IdeaRecipients.recipientId = @recipientId
+        """
+
+        let! ideas = env.db.query sql (dict ["recipientId" => recipientId] |> Some)
+        return ideas |> Result.map (List.map toIdea)
+    }
