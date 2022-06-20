41f88fa49cffa57f01fbd6c49cd54761caf0ba5b
ldscavo <logan@scavo.dev>
2022-06-20 17:29:40 -0400

    Add placeholder idea view

---
+++ src/GiftBudget/Ideas/IdeasViews.fs
@@ -0,0 +1,18 @@
+﻿module Ideas.Views
+
+open Giraffe.ViewEngine
+open Saturn
+open Ideas
+
+let ideaCard ctx (idea: Idea) =
+    div [_class "card"] [
+        div [_class "card-content"] [
+            div [_class "media"] [
+                div [_class "media-content"] [
+                    p [_class "title is-4"] [
+                        str idea.Text
+                    ]                        
+                ]
+            ]
+        ]
+    ]
