7717c2e8838fced0ef685b0c2145a697120f78ea
ldscavo <logan@scavo.dev>
2022-07-27 21:43:02 -0400

    Add a file for shared view components

---
+++ src/GiftBudget/Templates/Components.fs
@@ -0,0 +1,19 @@
+﻿module Components
+
+open Giraffe.ViewEngine
+open Giraffe.ViewEngine.Htmx
+open Saturn
+
+let icon id =
+    span [_class "icon"] [
+        i [_class $"fas %s{id}"] []
+    ]
+
+let modalLink href content =
+    a
+        [ _class "modal-link"
+          _href href
+          _hxGet href
+          _hxTarget "#modal-content"
+          _hxTrigger "click" ]
+        content
